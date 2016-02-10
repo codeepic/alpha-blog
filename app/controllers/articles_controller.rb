c9f3c5d8db1516883a12d1ea98ef7ce805813791
xavier <ksaweryglab@gmail.com>
2016-02-10 07:32:09 +0000

    added upcate, create and show actions to ArticlesController, added views, linked them using path helper and moved form to its own partial from new and edit views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,30 @@
 class ArticlesController < ApplicationController
   def index
+    puts 'I am here'
     @articles = Article.all
+    puts "articles #{@articles}"
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article updated successfully'
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
   end
 
   def create
@@ -12,7 +32,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      flash[:notice] = "Article was succcessfully created"
+      flash[:notice] = 'Article was successfully created'
       redirect_to article_path(@article)
     else
       render 'new' # you can also write render :new
