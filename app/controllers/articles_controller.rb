7ff5e1e6c23b08c1cc6eefd0653017583082ccb6
xavier <ksaweryglab@gmail.com>
2016-02-15 23:43:52 +0000

    styled form, notice and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
   def update
     # @article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = 'Article updated successfully'
+      flash[:success] = 'Article updated successfully'
       redirect_to article_path(@article)
     else
       render :edit
@@ -32,7 +32,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      flash[:notice] = 'Article was successfully created'
+      flash[:success] = 'Article was successfully created'
       redirect_to article_path(@article)
     else
       render 'new' # you can also write render :new
@@ -42,7 +42,7 @@ class ArticlesController < ApplicationController
   def destroy
     # @article = Article.find(params[:id])
     @article.destroy
-    flash[:notice] = 'Article deleted successfully.'
+    flash[:danger] = 'Article deleted successfully.'
     redirect_to articles_path # or redirect_to :index
   end
 
