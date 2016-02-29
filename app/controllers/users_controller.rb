cc944986465c03410565119e930267f90b1b133a
xavier <ksaweryglab@gmail.com>
2016-02-29 08:00:09 +0000

    added more user views and controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,10 +23,14 @@ class UsersController < ApplicationController
 
     if @user.update(user_params)
       flash[:success] = 'Successfully updated your account'
-      redirect_to articles_path
+      redirect_to user_path(@user) #articles_path
     else
       render :edit
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
